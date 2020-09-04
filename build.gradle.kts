88b36bd27c1f846cde4cd8f385bf009bc92ffaba
Hågen Hasle <haagenhasle@gmail.com>
2020-09-04 09:15:19 +0200

    Example showing how same selection sets for the same object leads to multiple Kotlin classes

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+val graphqlKotlinClientVersion = "3.6.1"
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+plugins {
+    kotlin("jvm") version "1.3.72"
+    id("com.expediagroup.graphql") version "3.6.1"
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.expediagroup:graphql-kotlin-client:$graphqlKotlinClientVersion")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+graphql {
+    client {
+        sdlEndpoint = "https://navikt.github.io/pdl/pdl-api-sdl.graphqls"
+        packageName = "pdl.generated"
+        allowDeprecatedFields = false
+        queryFiles = mutableListOf(file("${project.projectDir}/src/main/resources/hentPerson.graphql")
+        )
+    }
+}
